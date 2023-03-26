2be50a0c0bb829672ee739f9f429f6881c14f674
federico <federico.jurnet@globallogic.com>
2023-03-26 15:32:20 -0300

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'advisor-proyect',
+    appId: '1:194930359781:web:32e0ac8116070a5831430c',
+    storageBucket: 'advisor-proyect.appspot.com',
+    apiKey: 'AIzaSyAQS9csJgXuBXaYhu9QjNHz7rFvL8fo7Gg',
+    authDomain: 'advisor-proyect.firebaseapp.com',
+    messagingSenderId: '194930359781',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'advisor-proyect',
+    appId: '1:194930359781:web:32e0ac8116070a5831430c',
+    storageBucket: 'advisor-proyect.appspot.com',
+    apiKey: 'AIzaSyAQS9csJgXuBXaYhu9QjNHz7rFvL8fo7Gg',
+    authDomain: 'advisor-proyect.firebaseapp.com',
+    messagingSenderId: '194930359781',
+  },
   production: false
 };
 
